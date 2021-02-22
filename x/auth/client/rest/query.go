8bd9938e800ba33a91c5288b0a8bf02192ec6a73
Robert O'Leary <jatochnietdans@gmail.com>
2021-02-22 19:28:11 +0100

    Add order by print

--- x/auth/client/rest/query.go
+++ x/auth/client/rest/query.go
@@ -99,6 +99,8 @@ func QueryTxsRequestHandlerFn(clientCtx client.Context) http.HandlerFunc {
 			return
 		}
 
+		fmt.Println("Order by:", r.FormValue("order_by"))
+
 		searchResult, err := authclient.QueryTxsByEvents(clientCtx, events, page, limit, r.FormValue("order_by"))
 		if rest.CheckInternalServerError(w, err) {
 			return
